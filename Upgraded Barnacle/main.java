d20fd3b6e3b2e7027c3a3b690b8a2c8c7e258d34
Minkampel <emil@grasskamp.de>
2024-05-26 13:23:31 +0200

    Commit from 2024-05-26_13:23:31

---
+++ Upgraded Barnacle/main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Main class of the game
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+    public static void main(String[] args) {
+        System.out.println("yay");
+    }
+       
+
+}
